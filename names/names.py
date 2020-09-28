b9f4953b72789aa673c7c01d0005144a5d1371df
Cody Caro <codycaro@Codys-MBP.home>
2020-09-27 20:55:25 -0700

    Got close to figuring out the names portion

--- names/names.py
+++ names/names.py
@@ -1,81 +1,38 @@
 import time
+from collections import Counter
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+"""
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+"""
+for name in names_2:
+    names_1.append(name)
+
+all_name_counts = Counter(names_1)
+
+duplicates = [k for k, v in all_name_counts.items() if v > 1]
+# """
 
 end_time = time.time()
+
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
 
-
-class Node:
-    def __init__(self, val):
-        self.value = val
-        self.leftChild = None
-        self.rightChild = None
-
-    def insert(self, data):
-        if self.value == data:
-            return False
-        elif self.value > data:
-            if self.leftChild:
-                return self.leftChild.insert(data)
-            else:
-                self.leftChild = Node(data)
-                return True
-        else:
-            if self.rightChild:
-                return self.rightChild.insert(data)
-            else:
-                self.rightChild = Node(data)
-                return True
-
-    def find(self, data):
-        if(self.value == data):
-            return True
-        elif self.value > data:
-            if self.leftChild:
-                return self.leftChild.find(data)
-            else:
-                return False
-        else:
-            if self.rightChild:
-                return self.rightChild.find(data)
-            else:
-                return False
-
-
-class Tree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, data):
-        if self.root:
-            return self.root.insert(data)
-        else:
-            self.root = Node(data)
-            return True
-
-    def find(self, data):
-        if self.root:
-            return self.root.find(data)
-        else:
-            return False
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
